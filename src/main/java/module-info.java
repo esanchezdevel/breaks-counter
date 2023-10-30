b2bed6da70b9651e052ebeae30284b6d72dcba3e
Enrique Sanchez <esanchez.devel@gmail.com>
2023-10-30 20:55:47 +0100

    esanchezdevel/breaks-counter#7 feat: changes to be able to build the application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module breaksCounter {
   requires javafx.controls;
   requires javafx.fxml;
   requires javafx.media;
-  requires org.apache.logging.log4j;
 
   opens com.esanchez.devel.breakscounter.window to javafx.fxml;
   exports com.esanchez.devel.breakscounter.window;
